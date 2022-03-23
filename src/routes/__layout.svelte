ce972370b6802e23446346344c255cd9206ac2fd
Yajat Vishwak <yajat472001@gmail.com>
2022-03-23 15:25:21 +0530

    bro

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,15 +2,14 @@
     import "../app.css";
   </script>
   <svelte:head>
-    <link href="http://fonts.cdnfonts.com/css/albegos" rel="stylesheet">
-    <link href="http://fonts.cdnfonts.com/css/rochaline" rel="stylesheet">
+      <title>Meghnaify</title>
+    <link href="https://fonts.cdnfonts.com/css/albegos" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/rochaline" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
 </svelte:head>
-  
 
-  <slot />
 
   <style global>    
     body{
