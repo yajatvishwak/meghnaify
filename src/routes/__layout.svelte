3ace969c46d246534227aa23e0bb219cf45c6d2e
Yajat Vishwak <yajat472001@gmail.com>
2022-03-24 13:53:44 +0530

    ratelimit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,23 @@
 <script>
-    import "../app.css";
-  </script>
-  <svelte:head>
-      <title>Meghnaify</title>
-    <link href="https://fonts.cdnfonts.com/css/albegos" rel="stylesheet">
-    <link href="https://fonts.cdnfonts.com/css/rochaline" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+  import "../app.css";
+</script>
+
+<svelte:head>
+  <title>Meghnaify</title>
+  <link href="https://fonts.cdnfonts.com/css/albegos" rel="stylesheet" />
+  <link href="https,://fonts.cdnfonts.com/css/rochaline" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+    rel="stylesheet"
+  />
 </svelte:head>
 
 <slot />
-  <style global>    
-    body{
-        font-family: 'Rock 3D', cursive;
-    }
-    
-  </style>
+
+<style global>
+  body {
+    font-family: "Rock 3D", cursive;
+  }
+</style>
